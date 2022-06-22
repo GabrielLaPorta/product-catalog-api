0b444e1cb0ac307d0a2c710cc8a657ee520e98e8
Gabriel de La Porta <gabrieldelaporta1@gmail.com>
2022-06-22 18:53:16 -0300

    Validation if has authorization in validate-token middleware

--- middleware/validate-token.js
+++ middleware/validate-token.js
@@ -3,18 +3,22 @@ require("dotenv").config();
 const secretKey = process.env["SECRET_KEY"];
 
 module.exports = (req, res, next) => {
-    const token = req.get('Authorization').split(" ")[1];
-    if(token) {
-        try{
-            const payload = jwt.verify(token, secretKey);
-            console.log("[ValidaToken] Payload",payload);
-            next();
+    if(req.get('Authorization')){
+        const token = req.get('Authorization').split(" ")[1];
+        if(token) {
+            try{
+                const payload = jwt.verify(token, secretKey);
+                console.log("[ValidaToken] Payload",payload);
+                next();
+            }
+            catch(err){
+                res.status(403).json({mensagem:"Sem Token de acesso"});
+            }        
         }
-        catch(err){
+        else {
             res.status(403).json({mensagem:"Sem Token de acesso"});
-        }        
-    }
-    else {
+        }
+    } else {
         res.status(403).json({mensagem:"Sem Token de acesso"});
     }
 };
