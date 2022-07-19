e061a00192710d1f717af041aed228eae92960f8
Gabriel de La Porta <gabrieldelaporta1@gmail.com>
2022-07-19 00:51:58 -0300

    Ajust cors age

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Max-Age", "86400");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 
